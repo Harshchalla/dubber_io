1d0e62d532466d702ae82d2c5d17f151bb0b136f
Rajat Pawar <rajat.pawar100@gmail.com>
2024-09-08 16:45:25 -0700

    initial commit

--- main.py
+++ main.py
@@ -1,35 +1,55 @@
-import dataclasses
+import replicate
+import requests
+from moviepy.editor import VideoFileClip
 
-@dataclasses.dataclass
-class UnitTranscription:
-    start_timestamp: int
-    end_timestamp: int
-    content: text
-
-
-def transcribe(audio_filepath: str) -> list[UnitTranscription]:
-    # audio in wav
-    pass
-
-def translate(captions: list[UnitTranscription]) -> list[UnitTranscription]:
-    pass
-
-def text_to_speech(translated_captions: list[UnitTranscription], original_audio_filepath: str) -> str:
-    pass
 
 def lipsync(translated_audio_path: str, video_path: str) -> str:
     pass
 
 
-def main(video_filepath: str) -> str:
-    # preprocess
-    # transcribe
-    # translate
-    # text_to_speech
+def download_file(file_url, local_filename):
+    response = requests.get(file_url, stream=True)
+    if response.status_code == 200:
+        with open(local_filename, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+        print(f"File downloaded successfully as {local_filename}")
+    else:
+        print(f"Failed to download the file. Status code: {response.status_code}")
+    return local_filename
+
+
+def preprocess_clip(input_clip="input_video.mp4"):
+    video = VideoFileClip(input_clip)
+    audio = video.audio
+    audio.write_audiofile("input_audio.mp3")
+    audio.close()
+    video.close()
+
+
+def speech_to_speech_translation(input_audio_file, lang):
+    output = replicate.run(
+        "cjwbw/seamless_communication:668a4fec05a887143e5fe8d45df25ec4c794dd43169b9a11562309b2d45873b0",
+        input={
+            "task_name": "S2ST (Speech to Speech translation)",
+            "input_audio": open(input_audio_file, "rb"),  # Replace with your URI
+            "input_text_language": "None",
+            "max_input_audio_length": 60,
+            "target_language_text_only": "Norwegian Nynorsk",
+            "target_language_with_speech": lang
+        }
+    )
+    print(output)
+    return output["audio_output"]
+
+
+def main(lang: str) -> str:
+    preprocess_clip()
+    audio_output_filepath = speech_to_speech_translation("input_audio.mp3", lang)
+    download_file(audio_output_filepath, "output_audio.wav")
     # lipsync
-    # return
     pass
 
 
-    
-  
+if __name__ == "__main__":
+    main(lang="Japanese")
